ccb11d7fcee5a05c5b49986c52c856b4c2e82943
Shashvat Srivastava <shashvat.sri@gmail.com>
2023-02-23 15:17:11 -0500

    removed lines

--- arch/riscv/include/asm/sm_const.h
+++ arch/riscv/include/asm/sm_const.h
@@ -3,10 +3,7 @@
 #ifndef __ASM_SM_CONST_H
 #define __ASM_SM_CONST_H
 
-#define SBI_EXT_SM	0x8000000
 #define SBI_EXT_SM_CREATE 0x8000001
 #define SBI_EXT_SM_RESUME 0x8000002
-
-#define SBI_EXT_SM_SET_SHARED 0x0
 
 #endif
